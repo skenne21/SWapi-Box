91506d35e46a17a50d63bd2110b85af44c74058d
Sabrina Kennedy <skenne21@gmail.com>
2018-03-21 23:13:16 -0600

    eslint

--- src/Helpers/mockData.js
+++ src/Helpers/mockData.js
@@ -1,35 +1,35 @@
 const mockMovie = () => {
   return {
-      title: "The Empire Strikes Back",
-      episode_id: 5,
-      opening_crawl: 'It is a dark time for the...',
-      director: "Irvin Kershner", 
-      release_date: '1980-05-17',
-  } 
-}
+    title: "The Empire Strikes Back",
+    episodeId: 5,
+    openingCrawl: 'It is a dark time for the...',
+    director: "Irvin Kershner", 
+    release_date: '1980-05-17'
+  };
+};
 
 const mockApiPeople = () => {
   return [
-  {
-    name: "Luke Skywalker", 
-    height: "172", 
-    mass: "77", 
-    hair_color: 
-    "blond", 
-    skin_color: 
-    "fair", 
-    homeworld:"https://swapi.co/api/planets/1/",
-    species:["https://swapi.co/api/species/1/"]
-  },{
-    name: "C-3PO",
-    height: "167", 
-    mass: "75", 
-    hair_color: "n/a", 
-    skin_color: "gold",
-    homeworld:"https://swapi.co/api/planets/1/",
-    species:["https://swapi.co/api/species/1/"]
-  }]
-}
+    {
+      name: "Luke Skywalker", 
+      height: "172", 
+      mass: "77", 
+      hairColor: 
+      "blond", 
+      skinColor: 
+      "fair", 
+      homeworld:"https://swapi.co/api/planets/1/",
+      species:["https://swapi.co/api/species/1/"]
+    }, {
+      name: "C-3PO",
+      height: "167", 
+      mass: "75", 
+      hairColor: "n/a", 
+      skinColor: "gold",
+      homeworld:"https://swapi.co/api/planets/1/",
+      species:["https://swapi.co/api/species/1/"]
+    }];
+};
 
 const mockHomelandData = () => {
   return [
@@ -37,15 +37,15 @@ const mockHomelandData = () => {
       name: "Luke Skywalker",
       homeworld:"https://swapi.co/api/planets/1/",
       species:["https://swapi.co/api/species/1/"],
-      population: undefined,
+      population: undefined
     }, {
       name: "C-3PO",
       homeworld:"https://swapi.co/api/planets/1/",
       species:["https://swapi.co/api/species/1/"],
-      population: undefined,
+      population: undefined
     } 
-  ]
-}
+  ];
+};
 
 const mockSpecies = () => {
   return [
@@ -53,26 +53,25 @@ const mockSpecies = () => {
       name: "Luke Skywalker",
       homeworld: 'Somewhere',
       species: "https://swapi.co/api/species/1/",
-      population: 30000,
+      population: 30000
     }, {
       name: "C-3P0",
       homeworld: 'Somewhere',
       species: "https://swapi.co/api/species/1/",
-      population: 30000,
+      population: 30000
     }
-  ]
-}
+  ];
+};
 
 const mockApiPlanets = () => {
   return [{
     climate:"temperate",
-    created:"2014-12-10T11:35:48.479000Z",
     name: "Alderaan",
     population:"2000000000",
     residents:["https://swapi.co/api/people/5/", "https://swapi.co/api/people/68/", "https://swapi.co/api/people/81/"],
-    terrain:"grasslands, mountains",
-  }]
-}
+    terrain:"grasslands, mountains"
+  }];
+};
 
 
 
@@ -81,5 +80,5 @@ export default {
   mockApiPeople, 
   mockHomelandData, 
   mockSpecies,
-  mockApiPlanets, 
-}
+  mockApiPlanets
+};
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {shallow, mount } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
+
+describe('App shallow' , () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App/>)
+
+  })
+
+  it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  })
+
+  it('it has a default state empty object for films', () => {
+    expect(wrapper.state('film')).toEqual({})
+  })
+
+  it('it has a default state empty array for favorites', () => {
+    expect(wrapper.state('favorites')).toEqual([])
+  })
+
+  it('it has a default state empty array for favorites', () => {
+    expect(wrapper.state('favorites')).toEqual([])
+  })
+
+  it('it has a default state empty array for cards', () => {
+    expect(wrapper.state('cards')).toEqual([])
+  })
+
+  it('it has a default state error of false', () => {
+    expect(wrapper.state('error')).toEqual(false)
+  })
 });
+
+
